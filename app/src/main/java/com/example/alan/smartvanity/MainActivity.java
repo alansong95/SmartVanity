40bfb8e57c1bb2bc2d2c941ef378ed9cb47c8fe6
Alan Song <alansong95@gmail.com>
2018-03-31 22:15:56 -0400

    added bluetooth

--- app/src/main/java/com/example/alan/smartvanity/MainActivity.java
+++ app/src/main/java/com/example/alan/smartvanity/MainActivity.java
@@ -54,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             fragmentManager.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
         } else if (id == R.id.nav_controller) {
             fragmentManager.beginTransaction().replace(R.id.content_frame, new controllerFragment()).commit();
+        } else if (id == R.id.nav_bluetooth) {
+            fragmentManager.beginTransaction().replace(R.id.content_frame, new BluetoothFragment()).commit();
         } else if (id == R.id.nav_account) {
 
         } else if (id == R.id.nav_about_us) {
